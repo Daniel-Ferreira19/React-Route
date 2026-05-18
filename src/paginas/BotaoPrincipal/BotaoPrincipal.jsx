804758797a2be0644f1bde36090d0727c4a216f8
Daniel F <danielferreiranovo1@gmail.com>
2026-05-18 10:44:10 -0300

    adicionei um Botão Principal que passou como uma props

---
+++ src/paginas/BotaoPrincipal/BotaoPrincipal.jsx
@@ -0,0 +1,11 @@
+
+import style from "./BotaoPrincipal.module.css"
+export default function BotaoPrincipal({children , tamanho}){
+    return(
+         <button className={`
+            ${style.botao}
+            ${style[tamanho]}
+            `}>{children}</button>
+    ) 
+
+}
